7223b4af04b27346c238453140a45d202da43feb
Worldlyberry265 <74971527+Worldlyberry265@users.noreply.github.com>
2023-05-04 13:43:31 +0300

    Update Canny Optimised.c

--- Canny Optimised.c
+++ Canny Optimised.c
@@ -90,44 +90,117 @@ void calculate_gradients(unsigned char *img, int width, int height, float *magni
 }
 
 // Step 3: Non-maximum suppression
-__global__ void non_maximum_suppression_kernel(float *magnitude, float *orientation, unsigned char *edge_map, int width, int height)
+__global__ void non_maximum_suppression(float* input, float* output, int width, int height)
 {
     int x = threadIdx.x + blockIdx.x * blockDim.x;
     int y = threadIdx.y + blockIdx.y * blockDim.y;
-    if (x >= 1 && x < width - 1 && y >= 1 && y < height - 1) {
-        float mag = magnitude[y * width + x];
-        float angle = orientation[y * width + x];
-        float q = 255;
-        float r = 255;
 
-        // Check direction of edge
-        if ((angle < -M_PI / 8) || (angle >= M_PI / 8 && angle <= 3 * M_PI / 8)) {
-            q = magnitude[y * width + x + 1];
-            r = magnitude[y * width + x - 1];
-        } else if ((angle >= -3 * M_PI / 8 && angle < -M_PI / 8) || (angle >= 3 * M_PI / 8 && angle < M_PI / 8)) {
-            q = magnitude[(y - 1) * width + x];
-            r = magnitude[(y + 1) * width + x];
-        } else if (angle >= M_PI / 8 && angle < 3 * M_PI / 8) {
-            q = magnitude[y * width + x + 1];
-            r = magnitude[y * width + x - 1];
-        } else if (angle >= 3 * M_PI / 8 && angle < 5 * M_PI / 8) {
-            q = magnitude[(y + 1) * width + x - 1];
-            r = magnitude[(y - 1) * width + x + 1];
+    if (x < width && y < height)
+    {
+        int index = y * width + x;
+
+        // Compute the gradient direction
+        float dx = input[index + 1] - input[index - 1];
+        float dy = input[index + width] - input[index - width];
+        float gradient_direction = atan2(dy, dx);
+
+        // Round the gradient direction to one of four angles
+        gradient_direction = (gradient_direction < -M_PI_4) ? (M_PI + gradient_direction) : gradient_direction;
+        gradient_direction = (gradient_direction >= M_PI_4) ? (gradient_direction - M_PI) : gradient_direction;
+        gradient_direction = (gradient_direction >= -M_PI_4 && gradient_direction < M_PI_4) ? gradient_direction : (gradient_direction + M_PI);
+
+        // Determine the neighboring pixels in the gradient direction
+        int x1, y1, x2, y2;
+        if (gradient_direction >= -M_PI_4 && gradient_direction < M_PI_4)
+        {
+            x1 = 1; y1 = -1;
+            x2 = 1; y2 = 1;
+        }
+        else if (gradient_direction >= M_PI_4 && gradient_direction < 3 * M_PI_4)
+        {
+            x1 = -1; y1 = -1;
+            x2 = 1; y2 = 1;
+        }
+        else if (gradient_direction >= 3 * M_PI_4 || gradient_direction < -3 * M_PI_4)
+        {
+            x1 = -1; y1 = 0;
+            x2 = 1; y2 = 0;
+        }
+        else
+        {
+            x1 = -1; y1 = 1;
+            x2 = 1; y2 = -1;
         }
 
-        // Check if current pixel is a local maximum
-        if (mag >= q && mag >= r) {
-            edge_map[y * width + x] = (unsigned char) mag;
-        } else {
-            edge_map[y * width + x] = 0;
+        // Check if the current pixel is a local maximum in the gradient direction
+        float current_pixel = input[index];
+        float neighbor_pixel_1 = input[index + y1 * width + x1];
+        float neighbor_pixel_2 = input[index + y2 * width + x2];
+        if (current_pixel > neighbor_pixel_1 && current_pixel > neighbor_pixel_2)
+        {
+            output[index] = current_pixel;
+        }
+        else
+        {
+            output[index] = 0.0f;
         }
     }
 }
-// Step 3: Non-maximum suppression PART 2
-void non_maximum_suppression(float *magnitude, float *orientation, unsigned char *edge_map, int width, int height)
+
+// Step 4: double thresholding
+__global__ void double_thresholding(float* input, float* output, float low_threshold, float high_threshold, int width, int height)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x < width && y < height)
+    {
+        int index = y * width + x;
+
+        // Apply double thresholding
+        float pixel_value = input[index];
+        if (pixel_value >= high_threshold)
+        {
+            output[index] = 255.0f;
+        }
+        else if (pixel_value < low_threshold)
+        {
+            output[index] = 0.0f;
+        }
+        else
+        {
+            // Check if the pixel is connected to a strong edge
+            int is_connected_to_strong_edge = 0;
+            for (int j = -1; j <= 1; j++)
+            {
+                for (int i = -1; i <= 1; i++)
+                {
+                    if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height)
+                    {
+                        if (input[(y + j) * width + (x + i)] >= high_threshold)
+                        {
+                            is_connected_to_strong_edge = 1;
+                            break;
+                        }
+                    }
+                }
+                if (is_connected_to_strong_edge)
+                {
+                    break;
+                }
+            }
+            output[index] = (is_connected_to_strong_edge) ? 255.0f : 0.0f;
+        }
+    }
+}
+
+// CUDA Code
+void cudaManage(float *magnitude, float *orientation, float low_threshold, float high_threshold, unsigned char *edge_map, int width, int height)
 {
     // Allocate memory on device
     float *d_magnitude, *d_orientation;
+    float low_threshold = 50.0f;
+    float high_threshold = 150.0f;
     unsigned char *d_edge_map;
     cudaMalloc(&d_magnitude, width * height * sizeof(float));
     cudaMalloc(&d_orientation, width * height * sizeof(float));
@@ -141,8 +214,9 @@ void non_maximum_suppression(float *magnitude, float *orientation, unsigned char
     dim3 blockDim(16, 16);
     dim3 gridDim((width + blockDim.x - 1) / blockDim.x, (height + blockDim.y - 1) / blockDim.y);
 
-    // Call kernel function
+    // Call kernel functions
     non_maximum_suppression_kernel<<<gridDim, blockDim>>>(d_magnitude, d_orientation, d_edge_map, width, height);
+    double_thresholding<<<gridDim, blockDim>>>(edge_map, width, height, low_thresh, high_thresh);
 
     // Copy data from device to host
     cudaMemcpy(edge_map, d_edge_map, width * height * sizeof(unsigned char), cudaMemcpyDeviceToHost);
@@ -153,154 +227,120 @@ void non_maximum_suppression(float *magnitude, float *orientation, unsigned char
     cudaFree(d_edge_map);
 }
 
-// Step 4: double thresholding
-__global__ void double_thresholding_kernel(unsigned char *edge_map, int width, int height, float low_thresh, float high_thresh, int *queue_elements, int *queue_size, int *queue_front, int *queue_rear) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (i >= width * height) {
-        return;
-    }
-
-    // First pass: thresholding
-    if (edge_map[i] >= high_thresh) {
-        edge_map[i] = 255;
-    } else if (edge_map[i] < low_thresh) {
-        edge_map[i] = 0;
-    }
-
-    // Second pass: hysteresis thresholding
-    if (edge_map[i] == 255) {
-        int rear = atomicAdd(queue_rear, 1);
-        queue_elements[rear] = i;
-        atomicAdd(queue_size, 1);
-    }
-
-    __syncthreads();
-
-    while (*queue_size > 0) {
-        int idx = atomicAdd(queue_front, 1);
-        if (idx >= *queue_size) {
-            break;
-        }
-        int j = queue_elements[idx];
-        atomicSub(queue_size, 1);
-
-        for (int y = -1; y <= 1; y++) {
-            for (int x = -1; x <= 1; x++) {
-                if (y == 0 && x == 0) {
-                    continue;
-                }
-                int ix = j % width + x;
-                int iy = j / width + y;
-                if (ix < 0 || ix >= width || iy < 0 || iy >= height) {
-                    continue;
-                }
-                int idx2 = iy * width + ix;
-                if (edge_map[idx2] >= low_thresh && edge_map[idx2] < 255) {
-                    edge_map[idx2] = 255;
-                    int rear2 = atomicAdd(queue_rear, 1);
-                    queue_elements[rear2] = idx2;
-                    atomicAdd(queue_size, 1);
-                }
-            }
-        }
-
-        __syncthreads();
-    }
-}
-
-// Step 4: Non-maximum suppression PART 2
-void double_thresholding(unsigned char *edge_map, int width, int height, float low_thresh, float high_thresh) {
-    int *queue_elements, *queue_size, *queue_front, *queue_rear;
-    cudaMallocManaged(&queue_elements, width * height * sizeof(int));
-    cudaMallocManaged(&queue_size, sizeof(int));
-    cudaMallocManaged(&queue_front, sizeof(int));
-    cudaMallocManaged(&queue_rear, sizeof(int));
-    *queue_size = 0;
-    *queue_front = 0;
-    *queue_rear = -1;
-
-    int threads_per_block = 256;
-    int blocks_per_grid = (width * height + threads_per_block - 1) / threads_per_block;
-    double_thresholding_kernel<<<blocks_per_grid, threads_per_block>>>(edge_map, width, height, low_thresh, high_thresh, queue_elements, queue_size, queue_front, queue_rear);
-    cudaDeviceSynchronize();
-
-    cudaFree(queue_elements);
-    cudaFree(queue_size);
-    cudaFree(queue_front);
-    cudaFree(queue_rear);
-}
-
-
-
-
 // Step 5: Edge tracking by hysteresis
-void edge_tracking_mpi(unsigned char *edge_map, int width, int height, float low_thresh)
+void edge_tracking(unsigned char *edge_map, int width, int height, float low_thresh)
 {
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int weak = 25;
+    int strong = 255;   //CHANGE BACK TO 75 OR 60 
 
-    // Calculate sub-image dimensions
-    int sub_width = width / size;
-    int sub_height = height;
-    int sub_start = rank * sub_width;
-    int sub_end = (rank + 1) * sub_width;
-    if (rank == size - 1) {
-        sub_end = width;
+    // Initialize MPI
+    MPI_Init(NULL, NULL);
+
+    // Get the rank of the current process and the total number of processes
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Calculate the number of rows per process
+    int rows_per_proc = height / world_size;
+    int remainder = height % world_size;
+
+    // Calculate the starting and ending row for the current process
+    int start_row = world_rank * rows_per_proc;
+    int end_row = (world_rank + 1) * rows_per_proc;
+    if (world_rank == world_size - 1) {
+        end_row += remainder;
     }
 
-    // Allocate memory for sub-image and copy data
-    unsigned char *sub_edge_map = (unsigned char *) malloc(sub_width * sub_height * sizeof(unsigned char));
-    memcpy(sub_edge_map, edge_map + sub_start, sub_width * sub_height * sizeof(unsigned char));
+    // Allocate memory for the sub-image
+    int sub_height = end_row - start_row;
+    unsigned char *sub_edge_map = (unsigned char*) malloc(width * sub_height * sizeof(unsigned char));
 
-    // Apply edge tracking on sub-image
-    for (int y = 0; y < sub_height; y++) {
-        for (int x = 0; x < sub_width; x++) {
-            if (sub_edge_map[y * sub_width + x] >= low_thresh) {
-                // Start new edge
-                sub_edge_map[y * sub_width + x] = 255;
+    // Copy the sub-image from the main image to the sub-image
+    for (int y = start_row; y < end_row; y++) {
+        for (int x = 0; x < width; x++) {
+            sub_edge_map[(y - start_row) * width + x] = edge_map[y * width + x];
+        }
+    }
 
-                // Perform depth-first search to find connected edge pixels
-                int stack_x[100000], stack_y[100000], stack_top = 0;
-                stack_x[stack_top] = x;
-                stack_y[stack_top] = y;
-                while (stack_top >= 0) {
-                    int cx = stack_x[stack_top];
-                    int cy = stack_y[stack_top];
-                    stack_top--;
-                    for (int dy = -1; dy <= 1; dy++) {
-                        for (int dx = -1; dx <= 1; dx++) {
-                            if (cx + dx < 0 || cx + dx >= sub_width || cy + dy < 0 || cy + dy >= sub_height) {
-                                continue;
-                            }
-                            if (sub_edge_map[(cy + dy) * sub_width + (cx + dx)] >= low_thresh &&
-                                sub_edge_map[(cy + dy) * sub_width + (cx + dx)] < 255) {
-                                sub_edge_map[(cy + dy) * sub_width + (cx + dx)] = 255;
-                                stack_top++;
-                                stack_x[stack_top] = cx + dx;
-                                stack_y[stack_top] = cy + dy;
-                            }
-                        }
-                    }
+    // Edge tracking on the sub-image
+    for (int y = 1; y < sub_height - 1; y++) {
+        for (int x = 1; x < width - 1; x++) {
+            if (sub_edge_map[y * width + x] == weak) {
+                // Check if any of the 8 neighboring pixels are strong
+                if (sub_edge_map[(y - 1) * width + (x - 1)] == strong ||
+                    sub_edge_map[(y - 1) * width + x] == strong ||
+                    sub_edge_map[(y - 1) * width + (x + 1)] == strong ||
+                    sub_edge_map[y * width + (x - 1)] == strong ||
+                    sub_edge_map[y * width + (x + 1)] == strong ||
+                    sub_edge_map[(y + 1) * width + (x - 1)] == strong ||
+                    sub_edge_map[(y + 1) * width + x] == strong ||
+                    sub_edge_map[(y + 1) * width + (x + 1)] == strong) {
+                    sub_edge_map[y * width + x] = strong;
+                } else {
+                    sub_edge_map[y * width + x] = 0;
                 }
             }
         }
     }
 
-    // Gather sub-images to rank 0
-    if (rank == 0) {
-        for (int i = 1; i < size; i++) {
-            MPI_Recv(edge_map + i * sub_width, sub_width * sub_height, MPI_UNSIGNED_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    // Gather the results from all processes to the root process
+    if (world_rank == 0) {
+        // Allocate memory for the final edge map
+        unsigned char *final_edge_map = (unsigned char*) malloc(width * height * sizeof(unsigned char));
+
+        // Copy the sub-image from the current process to the final edge map
+        for (int y = start_row; y < end_row; y++) {
+            for (int x = 0; x < width; x++) {
+                final_edge_map[y * width + x] = sub_edge_map[(y - start_row) * width + x];
+            }
+        }       
+         // Receive the sub-images from the other processes and copy them to the final edge map
+    for (int i = 1; i < world_size; i++) {
+        // Calculate the starting and ending row for the current sub-image
+        int sub_start_row = i * rows_per_proc;
+        int sub_end_row = (i + 1) * rows_per_proc;
+        if (i == world_size - 1) {
+            sub_end_row += remainder;
         }
-    } else {
-        MPI_Send(sub_edge_map, sub_width * sub_height, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD);
+        int sub_height = sub_end_row - sub_start_row;
+
+        // Receive the sub-image from the current process
+        unsigned char *recv_buffer = (unsigned char*) malloc(width * sub_height * sizeof(unsigned char));
+        MPI_Recv(recv_buffer, width * sub_height, MPI_UNSIGNED_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // Copy the sub-image to the final edge map
+        for (int y = sub_start_row; y < sub_end_row; y++) {
+            for (int x = 0; x < width; x++) {
+                final_edge_map[y * width + x] = recv_buffer[(y - sub_start_row) * width + x];
+            }
+        }
+
+        // Free the receive buffer
+        free(recv_buffer);
     }
 
-    // Free memory
-    free(sub_edge_map);
+    // Copy the final edge map back to the original edge map
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            edge_map[y * width + x] = final_edge_map[y * width + x];
+        }
+    }
+
+    // Free the memory used by the final edge map
+    free(final_edge_map);
+} else {
+    // Send the sub-image to the root process
+    MPI_Send(sub_edge_map, width * sub_height, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD);
 }
 
+// Free the memory used by the sub-image
+free(sub_edge_map);
+
+// Finalize MPI
+MPI_Finalize();
+}
 
 
 int main()
@@ -369,6 +409,3 @@ fprintf(fp, "P5\n%d %d\n255\n", width, height);
     free(image_data);
     free(magnitude);
 }
-    
-   
-
